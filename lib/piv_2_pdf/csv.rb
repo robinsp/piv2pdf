139368e8608403731e7bbdae3ca1f4f56c408787
Robin Spainhour <robin@robinspainhour.com>
2010-09-23 21:41:25 +0200

    Use fastercsv to parse tracker export

--- lib/piv_2_pdf/csv.rb
+++ lib/piv_2_pdf/csv.rb
@@ -1,4 +1,11 @@
+require 'fastercsv'
+
 module Piv2Pdf
   class Csv
+    attr_reader :items
+    
+    def initialize(csv_filename)
+      @items = FasterCSV.table(csv_filename)
+    end
   end
 end
--- spec/lib/piv_2_pdf/csv_spec.rb
+++ spec/lib/piv_2_pdf/csv_spec.rb
@@ -5,8 +5,9 @@ require 'piv_2_pdf/csv'
 describe Piv2Pdf::Csv, "using canned csv file" do
   let(:filename) { "#{SPEC_RESOURCES}/sample.csv" }
 
-  it "should have 10 items"
+  it "should have 10 items" do
+    csv = Piv2Pdf::Csv.new(filename)
+    csv.should have(9).items
+  end
 
-  
 end
-
