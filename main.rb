392b95b0d9ea984252c042b103c54c711511ed2d
Robin Spainhour <robin.spainhour@deltaprojects.se>
2010-09-24 16:47:35 +0200

    Added pdfkit

--- main.rb
+++ main.rb
@@ -1,9 +1,26 @@
 require 'rubygems'
 
+require 'pdfkit'
+
 require 'lib/piv_2_pdf/csv'
 require 'lib/piv_2_pdf/tracker_item'
 require 'lib/piv_2_pdf/renderer'
 
+PDFKit.configure do |config|
+  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+  config.default_options = {
+    :page_size => 'A5'
+  }
+end
+
+
+
 csv = Piv2Pdf::Csv.new("/home/robin/src/piv2pdf/spec/res/sample.csv")
 
-puts Piv2Pdf::Renderer.new(csv.items).render
+html = Piv2Pdf::Renderer.new(csv.items).render
+kit = PDFKit.new(html)
+#kit.stylesheets << '/path/to/pdf.css'
+kit.to_pdf # inline PDF
+kit.to_file('/home/robin/tmp/rs.pdf')
+
+
